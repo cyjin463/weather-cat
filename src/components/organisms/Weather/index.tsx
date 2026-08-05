a7f9d9f14b78b29dad9609da720c670518428d23
최영진 <cyj@choeyeongjin-ui-noteubug.local>
2026-08-05 15:55:23 +0900

    [WC-11] 오늘읠 날씨 시간별 데이터 화면 작업

--- src/components/organisms/Weather/index.tsx
+++ src/components/organisms/Weather/index.tsx
@@ -1,17 +1,19 @@
 import { useWeatherStore } from "@/stores/weather";
 import { getWeatherImage } from "@/utils/weather";
-import { FlatList, Image, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 
 const Weather = () => {
+  const [tab, setTab] = useState<"today" | "wddk">("today");
   const { weatherData } = useWeatherStore();
   const { temperature: currentTemperature } = weatherData?.current ?? {};
+  console.log(weatherData?.hourlyToday);
 
   if (!weatherData) {
     return null;
   }
 
   const checkIsToday = (date: string) => {
-    // 날씨 API(timezone=auto)와 같은 좌표 기준 타임존으로 "오늘" 비교
     const today = new Date().toLocaleDateString("en-CA", {
       timeZone: weatherData.timezone || "Asia/Seoul",
     });
@@ -54,6 +56,37 @@ const Weather = () => {
         >{currentTemperature}</Text>
       </View>
       <View style={{ width: "100%", height: 1, backgroundColor: "#A7A7A7", marginVertical: 5 }} />
+     <View style={{ width: "100%", justifyContent: "flex-end", flexDirection: "row", gap: 10 }}>
+      <Pressable onPress={() => setTab("today")} style={{paddingVertical: 2, paddingHorizontal: 5, borderWidth: 1, borderRadius: 10}}>
+        <Text>오늘</Text>
+      </Pressable>
+      <Pressable onPress={() => setTab("wddk")} style={{paddingVertical: 2, paddingHorizontal: 5, borderWidth: 1, borderRadius: 10}}>
+        <Text>이번주</Text>
+      </Pressable>
+     </View>
+     {tab === "today" && (
+      <View style={{ width: "100%", paddingTop: 5 }}>
+        <FlatList
+         data={weatherData?.hourlyToday}
+         keyExtractor={(item) => item.hour.toString()}
+         horizontal
+         showsHorizontalScrollIndicator={false}
+         style={{ width: "100%" }}
+         contentContainerStyle={{ gap: 12, paddingHorizontal: 15 }}
+         renderItem={({ item }) => (
+          <View style={{ alignItems: "center", gap: 4 }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>{item.hour}시</Text>
+            <Image source={getWeatherImage(item.weatherCode)} style={{ width: 80, height: 80, borderRadius: 10 }} />
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Text style={{ fontSize: 16, fontWeight: "bold", color: "#5A4A4C" }}></Text>
+              <Text style={{ fontSize: 16, fontWeight: "bold", color: "#F58CA0" }}>{item.temperature}</Text>
+            </View>
+          </View>
+         )}
+        />
+      </View>
+     )}
+     {tab === "wddk" && (
       <View style={{ width: "100%", paddingTop: 5 }}>
         <FlatList
          data={weatherData?.daily}
@@ -78,6 +111,7 @@ const Weather = () => {
          )}
         />
       </View>
+     )}
     </View>
   );
 };
